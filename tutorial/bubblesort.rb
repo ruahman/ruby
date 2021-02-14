5ccb5bcd77b37bc51594b6438778206f1430cdc9
diego <dego_vila@yahoo.com>
2021-02-14 01:13:00 -0400

    added sorting

---
+++ tutorial/bubblesort.rb
@@ -0,0 +1,22 @@
+
+def bubble_sort array
+    n = array.length
+
+    loop do
+        
+        swap = false
+
+        (n-1).times do |i|
+            if array[i] > array[i+1]
+                array[i], array[i+1] = array[i+1], array[i]
+                swap = true
+            end
+        end
+
+        break unless swap
+    end
+
+    array
+end
+
+p bubble_sort([10, 44, 33, 12, 45, 30, 23, 89, 22])
